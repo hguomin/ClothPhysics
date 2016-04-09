5c1f05e8b1af1674109df05605aecd6c76d2a637
Jakob Törmä Ruhl <jakob.ruhl@gmail.com>
2016-04-09 13:01:42 +0200

    code cleanup

--- ClothPhysics/main.cpp
+++ ClothPhysics/main.cpp
@@ -2,41 +2,20 @@
 #include <Windows.h>
 #include <GL\glew.h>
 #include <GL\freeglut.h>
-//#include <AntTweakBar.h>
 
 #include "Display.h"
-//#include "Basic_Shader.h"
-//#include "Phong_Shader.h"
-//#include "Mesh.h"
 #include "Texture.h"
 #include "Transform.h"
 #include "Camera.h"
 #include "Keyboard.h"
 #include "Mouse.h"
-#include "Skybox.h"
-//#include "GridMesh.h"
-//#include "Cloth2.h"
 #include "Cloth_GPU.h"
 
 #include <iostream>
-#include <algorithm>
-#include <stdlib.h>
-#include <time.h>
-
-#include "GLError.h"
-
-/*https://www.youtube.com/watch?v=RqRxhY6iLto */
-
-#define DESIRED_FPS 120.0f
-#define MAX_PHYSICS_STEP 6
-#define MS_PER_SECOND 1000.0f
-#define DESIRED_FRAME_TIME (MS_PER_SECOND/ DESIRED_FPS)
-#define MAX_DELTA_TIME 0.1f
-
-bool GPU = true;
 
 int main(int argc, char ** argv[])
 {
+	std::cout << "Init started" << std::endl;
 	Display display(800, 600, "TSBK07 Space");
 	
 	Texture texture("./textures/white.jpg");
@@ -45,32 +24,16 @@ int main(int argc, char ** argv[])
 	Camera camera(cameraStartPosition, 70.0f, display.GetAspectRation(), 0.01f, 1000.0f);
 	camera.SetForward(glm::vec3(0.1f, -0.4f, -0.9f));
 
-	Skybox sky;
-	sky.SkyboxInit("./textures/skybox/", "back.jpg", "front.jpg", "left.jpg", "right.jpg", "top.jpg", "bottom.jpg");
-	
 	Transform transform;
 	Keyboard keyboard;
 	Mouse mouse;
-	//Cloth2 cloth(8, 8, 75, 75);
 	Cloth_GPU cloth_gpu;
 	
-	float counter = 0.0f;
-
-	float previousTicks = (float)SDL_GetTicks();
-
-	srand((unsigned int)time(NULL));
-
 	SDL_Event sdl_event;
 
 	std::cout << "init complete" << std::endl;
 	while (!display.IsClosed())
 	{
-		//time handling
-		float currentTicks = (float)SDL_GetTicks();
-		float frameTime = currentTicks - previousTicks;
-		previousTicks = currentTicks;
-		float totalDeltaTime = frameTime / DESIRED_FRAME_TIME;
-
 		display.Clear(0, 0, 0, 1);
 
 		while (SDL_PollEvent(&sdl_event))
@@ -84,15 +47,12 @@ int main(int argc, char ** argv[])
 				mouse.HandleEvent(sdl_event, camera);
 			}
 		}
-		
 		const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
 		
 		keyboard.HandleEvent(currentKeyStates, camera);
 
 		cloth_gpu.Draw(transform, camera);
 		display.Update();
-		
-		counter += 0.001f;
 	}
 	return 0;
 }
